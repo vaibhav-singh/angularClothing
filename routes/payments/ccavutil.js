193c352bfd0367517fc2ee78a7cbdfde59eca7d6
shivanubhateja <shivanubhateja31@gmail.com>
2017-09-14 01:01:26 +0530

    aff:

--- routes/payments/ccavutil.js
+++ routes/payments/ccavutil.js
@@ -2,7 +2,7 @@ var crypto = require('crypto');
 exports.encrypt = function (plainText, workingKey) {
 	var m = crypto.createHash('md5');
     	m.update(workingKey);
-   	var key = m.digest().slice(0, 16);;
+   	var key = m.digest("binary").slice(0, 16);;
       	var iv = '\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f';	
 	var cipher = crypto.createCipheriv('aes-128-cbc', key, iv);
 	var encoded = cipher.update(plainText,'utf8','hex');
@@ -14,7 +14,7 @@ exports.encrypt = function (plainText, workingKey) {
 exports.decrypt = function (encText, workingKey) {
     	var m = crypto.createHash('md5');
     	m.update(workingKey)
-    	var key = m.digest().slice(0, 16);;
+    	var key = m.digest("binary").slice(0, 16);;
 	var iv = '\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f';	
 	var decipher = crypto.createDecipheriv('aes-128-cbc', key, iv);
     	var decoded = decipher.update(encText,'hex','utf8');
