daef01e6e71be3be291e491efa3ffd7dc58f6305
shivanubhateja <shivanubhateja31@gmail.com>
2017-09-13 22:27:51 +0530

    aaaaaaqa

--- app.js
+++ app.js
@@ -12,10 +12,11 @@ var admin = require('./routes/admin');
 var error = require('./routes/error');
 var users = require('./routes/users');
 var apiCalls = require('./routes/apiCalls');
-var paymentApi = require('./routes/payments/payments');
+var payment = require('./routes/payments/payments');
 
 var app = express();
 
+app.use(cors())
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -23,36 +24,16 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 // app.use(logger('dev'));
-// 
-// var http = require('http'),
-//     fs = require('fs'),
-//     ccav = require('./routes/payments/ccavutil'),
-//     qs = require('querystring'),
-//     ccavReqHandler = require('./routes/payments/ccavRequestHandler'),
-//     ccavResHandler = require('./routes/payments/ccavResponseHandler');
-
-// app.post('/ccavRequestHandler', function (request, response){
-// 	ccavReqHandler.postReq(request, response);
-// });
-
-
-// app.post('/ccavResponseHandler', function (request, response){
-//     ccavResHandler.postRes(request, response);
-// });
-
-
-
-app.use(cors())
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/users', users);
 var apiCall = app.use('/api', apiCalls);
-var admin = app.use('/admin', admin);
+var errorPage = app.use('/admin', admin);
 var errorPage = app.use('/notfound', error);
-// var payment = app.use('/payment', paymentApi);
+var payment = app.use('/payment', payment);
 var pageRequest = app.use('/', index);
 
 // catch 404 and forward to error handler
