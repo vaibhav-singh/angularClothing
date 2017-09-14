24c90d3d245aea557a6ea7d2d1637ebd7afb6a51
shivanubhateja <shivanubhateja31@gmail.com>
2017-09-14 20:22:03 +0530

    af

--- routes/payments/ccavResponseHandler.js
+++ routes/payments/ccavResponseHandler.js
@@ -2,6 +2,7 @@ var http = require('http'),
     fs = require('fs'),
     ccav = require('./ccavutil.js'),
     qs = require('querystring');
+    path = require('path');
 
 exports.postRes = function(request,response){
 	console.log("handle innnn")
@@ -27,7 +28,7 @@ exports.postRes = function(request,response){
             htmlcode = '<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><title>Response Handler</title></head><body><center><font size="4" color="blue"><b>Response Page</b></font><br>'+ pData +'</center><br></body></html>';
             response.writeHeader(200, {"Content-Type": "text/html"});
 	    // response.write(htmlcode);
-		res.sendFile(path.join(__dirname + '/../views/index.html'));
+		response.sendFile(path.join(__dirname + '/../views/index.html'));
 	    // response.end();
 	}); 	
 };
