14d0a777dd93d339a26d8e65039ae0691aa876bf
shivanubhateja <shivanubhateja31@gmail.com>
2017-09-14 20:27:05 +0530

    af

--- routes/payments/ccavResponseHandler.js
+++ routes/payments/ccavResponseHandler.js
@@ -28,7 +28,8 @@ exports.postRes = function(request,response){
             htmlcode = '<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><title>Response Handler</title></head><body><center><font size="4" color="blue"><b>Response Page</b></font><br>'+ pData +'</center><br></body></html>';
             response.writeHeader(200, {"Content-Type": "text/html"});
 	    // response.write(htmlcode);
-		response.sendFile(path.join(__dirname + '/views/index.html'));
+		console.log(path.join(__dirname + '/../../views/index.html'));
+		response.sendFile(path.join(__dirname + '/../../views/index.html'));
 	    // response.end();
 	}); 	
 };
