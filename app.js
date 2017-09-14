e84a0db366e2a190d6548493638fd80e88df1c42
shivanubhateja <shivanubhateja31@gmail.com>
2017-09-14 18:47:53 +0530

    af

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.set('view engine', 'jade');
 
 app.use(cors())
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
