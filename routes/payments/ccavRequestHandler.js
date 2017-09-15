9d14ee591dc4264222b02d0ad295d14c1ee5c361
shivanubhateja <shivanubhateja31@gmail.com>
2017-09-15 16:11:07 +0530

    adfa

--- routes/payments/ccavRequestHandler.js
+++ routes/payments/ccavRequestHandler.js
@@ -15,7 +15,11 @@ exports.postReq = function(request,response){
     console.log(data);
     console.log(typeof data);
     var a = data.toString('utf8');
-    console.log(typeof a)
+    console.log(a)
+    var JsonRes = JSON.parse('{"' + decodeURI(a).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}')
+		console.log(JsonRes);
+		console.log(typeof JsonRes);
+		console.log(JSON.stringify(JsonRes))
 	encRequest = ccav.encrypt(body,workingKey); 
 
 	formbody = '<form id="nonseamless" method="post" name="redirect" action="https://secure.ccavenue.com/transaction/transaction.do?command=initiateTransaction"/> <input type="hidden" id="encRequest" name="encRequest" value="' + encRequest + '"><input type="hidden" name="access_code" id="access_code" value="' + accessCode + '"><script language="javascript">document.redirect.submit();</script></form>';
