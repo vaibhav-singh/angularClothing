ecdc04e4208b811221279de8636bc312ce793ede
shivanubhateja <shivanubhateja31@gmail.com>
2017-11-27 18:14:48 +0530

    hotsellers

--- public/javascripts/home/home.js
+++ public/javascripts/home/home.js
@@ -1,4 +1,4 @@
-angular.module('mainApp').controller("homePageCtrl", ["$scope", '$timeout', '$rootScope', '$state', '$location', function($scope, $timeout, $rootScope, $state, $location){
+angular.module('mainApp').controller("homePageCtrl", ["$scope", '$timeout', '$rootScope', '$state', '$location', 'productService', function($scope, $timeout, $rootScope, $state, $location, productService){
     $rootScope.hideCartFromNavBar = false;
     $(document).ready(function () {
         $(".owl-carousel.homePage").owlCarousel({
@@ -21,10 +21,43 @@ angular.module('mainApp').controller("homePageCtrl", ["$scope", '$timeout', '$ro
         //     },
         //     offset: 200
         // })
-        $scope.owl = $('.owl-carousel');
+        $scope.owl = $('.owl-carousel.homePage');
+            
     });
+    $scope.getHotsellers = () => {
+        productService.getproductsToDisplay('men', 'topwear', 'Solids', '', 'ocselect', 1).then(function(response){
+            $scope.hotsellers = response.data.products;
+            $(document).ready(function () {
+                $('.owl-carousel.hotsellers').owlCarousel({
+                    // rtl:true,
+                    // loop:true,
+                    margin:10,
+                    nav:true,
+                                navText: ['<i class="fa fa-chevron-left" aria-hidden="true"></i>', '<i class="fa fa-chevron-right" aria-hidden="true"></i>'],
+
+                    responsive:{
+                        0:{
+                            items:2
+                        },
+                        600:{
+                            items:3
+                        },
+                        1000:{
+                            items:3
+                        }
+                    }
+                })        
+            })
+        }, function(response){
+            
+        });
+    }
+    $scope.getHotsellers();
     $scope.gotoUrl = function(url){
         $location.url(url);
+    }
+    $scope.openProduct = function(url, id, index){
+        $state.go('productDetails', {productId: id})
     }
     $timeout(function(){
         $scope.pageLoaded = true;
