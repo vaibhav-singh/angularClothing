6050c91848262bb83df6ef3b9c110ef661258a09
shivanubhateja <shivanubhateja31@gmail.com>
2018-03-04 14:08:55 +0530

    changed mail id for checkouts mail

--- services/commonServices.js
+++ services/commonServices.js
@@ -7,8 +7,9 @@ let transporter = nodemailer.createTransport({
   auth: {
     type: "OAuth2",
     user: "help.orangeclips@gmail.com",
-    refreshToken: "1/UO04dRKQL362VMd1CVppoPTGgRiY1DFpD_W2hVsZqW9402tx2zmXk21lwSRa2Pss",
-    accessToken: "ya29.GlvNBFrhxJtfI1DO7ajH9vf0YkiYS_BtZvqhaih3Zy7fO3n821YK3QASF3raS2EkqTVYs_uvIVzGHnqAqp24rDMqv0R_WucbOGPwZWOizy48r5ogdAj9WiTEmRwK",
+    // refreshToken: "1/UO04dRKQL362VMd1CVppoPTGgRiY1DFpD_W2hVsZqW9402tx2zmXk21lwSRa2Pss",
+    refreshToken: "1/KyYwU9iqOP6zkiK9nBgPDFLVJvZCd1fXdBvK7B1NBcg",
+    accessToken: "ya29.Glt0BQ77yLCl8fIhNGTO0Ff0V5iD0QZ8UOLC0kpWWmK7p05XmgpG9rHlYr8R3f38DT29fh6-5qBJvae3hbAcfUI-aKbwGp9aPxGrzykyURqHwnY2aDQnBPQdFTAs",
     clientId: "659815666715-ke8hhc7aqbaago8ehr90f8a7svj6v9tk.apps.googleusercontent.com",
     clientSecret: "ugwyEa-wW4l6kvQJ1nROM6vY"
   }
@@ -23,9 +24,10 @@ exports.sendMail = function(sendMailTo, subject, mailContent) {
   };
   transporter.sendMail(mailOptions, function(error, info) {
     if (error){
+      console.log(error)
     } 
     else{
-      // console.log("sent")
+      console.log("sent")
     } 
   });
 };
