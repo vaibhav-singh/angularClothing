4cc84a7a70c3aac22fc70c7222d126e55f42c02e
shivanubhateja <shivanubhateja31@gmail.com>
2018-03-04 17:48:50 +0530

    changed mail id for checkouts mail

--- services/commonServices.js
+++ services/commonServices.js
@@ -6,10 +6,6 @@ let transporter = nodemailer.createTransport({
   secure: true,
   auth: {
     type: "OAuth2",
-    user: "help.orangeclips@gmail.com",
-    // refreshToken: "1/UO04dRKQL362VMd1CVppoPTGgRiY1DFpD_W2hVsZqW9402tx2zmXk21lwSRa2Pss",
-    refreshToken: "1/KyYwU9iqOP6zkiK9nBgPDFLVJvZCd1fXdBvK7B1NBcg",
-    accessToken: "ya29.Glt0BQ77yLCl8fIhNGTO0Ff0V5iD0QZ8UOLC0kpWWmK7p05XmgpG9rHlYr8R3f38DT29fh6-5qBJvae3hbAcfUI-aKbwGp9aPxGrzykyURqHwnY2aDQnBPQdFTAs",
     clientId: "659815666715-ke8hhc7aqbaago8ehr90f8a7svj6v9tk.apps.googleusercontent.com",
     clientSecret: "ugwyEa-wW4l6kvQJ1nROM6vY"
   }
@@ -20,7 +16,13 @@ exports.sendMail = function(sendMailTo, subject, mailContent) {
     to: sendMailTo, // list of receivers
     subject: subject+" ✔", // Subject line
     text: "Email", // plaintext body
-    html: mailContent // html body
+    html: mailContent, // html body
+    auth: {
+      user: "help.orangeclips@gmail.com",
+      refreshToken: "1/itxDTganF9gUCttFugwsd-HSP3UfKlPB4WvMmcByOqI",
+      accessToken: "ya29.Glt0Be_Priud_yEkSdohK9607q4tKrEYIGk3n4ekTKBU8Y1Rc0IqNtd05J3KzqEUs90ytr6K8F4XEqNGi-UlYqAPuXBdTpzsbNaBL_UFkscKdvgxjL-NHGRO46f3",   
+      expires      : 1494388182480
+    }
   };
   transporter.sendMail(mailOptions, function(error, info) {
     if (error){
